8db52a8b7d1a13ba2edf4175bbf323e833b33370
Joe Cheng <joe@rstudio.com>
2018-03-09 10:35:41 -0800

    Initial commit

---
+++ DESCRIPTION
@@ -0,0 +1,6 @@
+Package: placeholder
+Type: Book
+Title: Does not matter.
+Version: 0.0.1
+Imports: bookdown
+Remotes: rstudio/bookdown
---
+++ _deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+[ -z "${GITHUB_PAT}" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+
+git config --global user.email "joe@rstudio.com"
+git config --global user.name "Joe Cheng"
+
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+cd book-output
+cp -r ../_book/* ./
+git add --all *
+git commit -m"Update the book" || true
+git push -q origin gh-pages
