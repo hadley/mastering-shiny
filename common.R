8f1bdbd08f6e68a1f8c40237bde718c9f4ea717c
Hadley Wickham <h.wickham@gmail.com>
2019-06-29 11:06:27 -0500

    Remove outdated read_csv

--- common.R
+++ common.R
@@ -9,26 +9,6 @@ knitr::opts_chunk$set(
 
 options(digits = 3)
 
-# Masks readr::read_csv, and performs persistent download caching
-read_csv <- function(file, ...) {
-  if (grepl("^https?://", file)) {
-    url <- file
-    ext <- tools::file_ext(url)
-    if (nchar(ext) > 0) {
-      ext <- paste0(".", ext)
-    }
-    hash <- digest::digest(url, "sha1")
-
-    dir.create("_download_cache", showWarnings = FALSE)
-
-    file <- file.path("_download_cache", paste0(hash, ext))
-    if (!file.exists(file)) {
-      download.file(url, file, method = "libcurl")
-    }
-  }
-  readr::read_csv(file, ...)
-}
-
 
 # Custom printing ---------------------------------------------------------
 knit_print <- knitr::knit_print
