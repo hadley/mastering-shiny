d73503b82a81f6492b5d42979ed4ae77be6062a2
Hadley Wickham <h.wickham@gmail.com>
2019-07-25 08:39:53 -0500

    Put prototype app in a file

---
+++ neiss/prototype.R
@@ -0,0 +1,53 @@
+library(tidyverse)
+library(vroom)
+library(shiny)
+
+injuries <- vroom::vroom("injuries.tsv.gz")
+products <- vroom::vroom("products.tsv")
+population <- vroom::vroom("population.tsv")
+
+ui <- fluidPage(
+  fluidRow(
+    column(6,
+      selectInput("code", "Product", setNames(products$prod_code, products$title))
+    )
+  ),
+  fluidRow(
+    column(4, tableOutput("diag")),
+    column(4, tableOutput("body_part")),
+    column(4, tableOutput("location"))
+  ),
+  fluidRow(
+    column(12, plotOutput("age_sex"))
+  )
+)
+
+server <- function(input, output, session) {
+  selected <- reactive(injuries %>% filter(prod_code == input$code))
+
+  output$diag <- renderTable(
+    selected() %>% count(diag, wt = weight, sort = TRUE)
+  )
+  output$body_part <- renderTable(
+    selected() %>% count(body_part, wt = weight, sort = TRUE)
+  )
+  output$location <- renderTable(
+    selected() %>% count(location, wt = weight, sort = TRUE)
+  )
+
+  summary <- reactive({
+    selected() %>%
+      count(age, sex, wt = weight) %>%
+      left_join(population, by = c("age", "sex")) %>%
+      mutate(rate = n / population * 1e4)
+  })
+
+  output$age_sex <- renderPlot({
+    summary() %>%
+      ggplot(aes(age, n, colour = sex)) +
+      geom_line() +
+      labs(y = "Estimated number of injuries")
+  })
+}
+
+shinyApp(ui, server)
