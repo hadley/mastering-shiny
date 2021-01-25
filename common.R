263039890e3f103cbcac73b97426027dfe10fffb
Hadley Wickham <h.wickham@gmail.com>
2021-01-25 08:55:04 -0600

    Feedback from reviewers

--- common.R
+++ common.R
@@ -1,6 +1,15 @@
 # load shiny first to avoid any conflict messages later
 library(shiny)
 
+# Don't run apps when knitting
+shinyApp <- function(...) {
+  if (isTRUE(getOption("knitr.in.progress"))) {
+    invisible()
+  } else {
+    shiny::shinyApp(...)
+  }
+}
+
 knitr::opts_chunk$set(
   comment = "#>",
   collapse = TRUE,
