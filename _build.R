a1a7b4ad0941afc1633d2594ae1e7d801ecf3abe
Hadley Wickham <h.wickham@gmail.com>
2021-01-27 07:17:19 -0600

    Switch to regexp based replacement

--- _build.R
+++ _build.R
@@ -1,33 +1,48 @@
 # https://atlas.oreilly.com/oreillymedia/mastering-shiny
+library(tidyverse)
 library(fs)
-library(dplyr)
 
+dir_delete("_oreilly")
 dir_create("_oreilly")
 Sys.setenv(CI = "true") # don't rebuild demos
 
 chapters <- setdiff(yaml::read_yaml("_bookdown.yml")$rmd_files, "index.Rmd")
 
 # Build book --------------------------------------------------------------
-# Clean out caches, figures, and previously built files
 
-asciidoc <- rmarkdown::output_format(
-  knitr = list(), # already set in common
-  keep_md = TRUE,
-  pandoc = rmarkdown::pandoc_options(
-    to = "asciidoc",
-    from = rmarkdown::from_rmarkdown(implicit_figures = FALSE)
-  ),
-  clean_supporting = FALSE
+format <- rmarkdown::md_document(variant = "markdown-fenced_code_attributes-raw_attribute", ext = ".asciidoc")
+format$pandoc$args <- c(format$pandoc$args, "--wrap=none")
+
+
+# TODO: use callr::r() to run in clean session
+chapters[[3]] %>% walk(
+  rmarkdown::render,
+  format,
+  output_dir = "_oreilly",
+  quiet = TRUE
 )
 
-rmarkdown::render("basic-app.Rmd", asciidoc, output_dir = "_oreilly")
-rmarkdown::render("basic-ui.Rmd", asciidoc, output_dir = "_oreilly")
+regexp <- googlesheets4::read_sheet("1b3j_fgnN19uvIG7XhSS7zepBTZO5vhbuEOXB5a_oT4Q")
+regexp$Pattern <- gsub("\\\\n", "\n", regexp$Pattern)
+regexp$Replacement <- gsub("\\\\n", "\n", regexp$Replacement)
 
-# why aren't demos producing figures?
-# why are heading ids and figures getting lost?
-# captions?
-# need to render in clean session
-# how to handle sidebars
+munge_file <- function(path) {
+  file <- read_file(path)
+
+  for (i in seq_len(nrow(regexp))) {
+    file <- str_replace_all(file, regex(regexp$Pattern[[i]], multiline = TRUE), regexp$Replacement[[i]])
+  }
+
+  write_file(file, path)
+}
+
+asciidoc <- dir_ls("_oreilly/", glob = "*.asciidoc")
+asciidoc %>% walk(munge_file)
+
+
+# indented code blocks - done
+# captions - done
+# parts - ([[unique_part_id]]\n[part])
 
 # Copy additional resources -----------------------------------------------
 library(dplyr)
