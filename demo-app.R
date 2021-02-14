05012e8854d297c9bf46cd425f78432e400ed11c
Hadley Wickham <h.wickham@gmail.com>
2021-02-14 08:33:33 -0600

    Start working on action feedback demos

--- demo-app.R
+++ demo-app.R
@@ -2,9 +2,6 @@ library(shiny)
 attach(readRDS("data.rds"))
 
 lapply(`_packages`, library, character.only = TRUE)
-for (prefix in names(`_resources`)) {
-  shiny::addResourcePath(prefix, `_resources`[[prefix]])
-}
 
 if (!exists("_server")) {
   `_server` <- function(input, output, session) {}
